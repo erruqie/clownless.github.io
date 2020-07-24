6623fa4fbe4a0d37172d7a538f64cd532a178048
m4x3r1337 <m4x3r1337@yandex.ru>
2020-07-24 17:06:10 +0400

    aye

---
+++ assets/js/title.js
@@ -0,0 +1,11 @@
+var text = ["$", " $", "r $", "3r $", "x3r $", "4x3r $", "m4x3r $", "m4x3 $", "m4x $","m4 $","m $"];
+var counter = 0;
+var inst = setInterval(change, 300);
+
+function change() {
+    document.title = text[counter];
+    counter++;
+    if (counter >= text.length) {
+        counter = 0;
+    }
+}
